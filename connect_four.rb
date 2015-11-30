7dc277feb100517a694aed20fea16780ab5464a4
Jerome Sardoma <Jerome@jeromes-mbp.fios-router.home>
2015-11-30 14:16:09 -0800

    first commit

---
+++ connect_four.rb
@@ -0,0 +1,131 @@
+Player = Struct.new(:number, :name, :marker)
+
+class ConnectFour
+  attr_reader :board, :player1, :player2
+  
+  def initialize
+    @board = Array.new(6) { Array.new(7) { "_" } }
+    @player1 = Player.new(1, "Player 1", "X")
+    @player2 = Player.new(2, "Player 2", "O")
+  end
+  
+  def show_board
+    @board.reverse.each { |row| puts row.join }
+  end
+  
+  def game
+    player_number = 1
+    win_indicator = 0
+    until win_indicator == 1
+      show_board
+      puts "*** #{current_player(player_number).name}, which column will you drop into? ***"
+      input = gets.chomp
+      check_input(input)
+      drop_marker(input, player_number)
+      if win?(player_number)
+        win_indicator = 1
+      else
+        player_number = switch_player(player_number)
+      end     
+    end
+    show_board
+    puts "*** #{current_player(player_number).name} wins! Play again? ***"
+    play_again?
+  end
+  
+  def check_input(input)
+    if ( input =~ /\D/ || input.to_i < 1 || input.to_i > 8 )
+      puts "Invalid input. Please enter a single digit."
+      new_input = gets.chomp
+      check_input(new_input)
+    else
+      input
+    end
+  end
+  
+  def drop_marker(input, player_number, row_counter = 0)
+    if row_counter == 6
+      row_counter = 0
+      puts "This column is full already. Select another column."
+      new_input = gets.chomp
+      drop_marker(check_input(new_input), player_number, row_counter)
+    elsif @board[row_counter][input.to_i - 1] == "_" 
+      @board[row_counter][input.to_i - 1] = current_player(player_number).marker
+    else
+      row_counter += 1
+      drop_marker(input, player_number, row_counter)
+    end
+  end
+  
+  def current_player(player_number)
+    player_number == 1 ? @player1 : @player2
+  end
+  
+  def switch_player(player_number)
+    player_number == 1 ? 2 : 1
+  end
+  
+  def win?(player_number)
+    # join elements into a string, then check for 4 of the current player's marker
+    winning_string = ""
+    4.times { winning_string << current_player(player_number).marker }
+    if (check_columns(winning_string, 7, @board) ||
+       check_rows(winning_string, 6, @board) ||
+       check_diagonals(winning_string) )
+      true
+    else
+      false
+    end
+  end
+  
+  def check_columns(string, number_of_columns, board, row_counter = 0)  
+    transpose = board.transpose
+    check_rows(string, number_of_columns, transpose, row_counter)
+  end
+  
+  def check_rows(string, number_of_rows, board, row_counter = 0)
+    if row_counter > number_of_rows - 1
+      false
+    elsif board[row_counter].join.include? string.to_s
+      true
+    else
+      row_counter += 1
+      check_rows(string, number_of_rows, board, row_counter)
+    end
+
+  end
+  
+  def check_diagonals(string)
+    diagonal_board = [
+      @board[0].slice(0..3),
+      @board[1].slice(0..4),
+      @board[2].slice(0..5),
+      @board[3].slice(1..6),
+      @board[4].slice(2..6),
+      @board[5].slice(3..6)
+    ]
+    2.times { diagonal_board[0].unshift("_") }
+    diagonal_board[1].unshift("_")
+    diagonal_board[4] << "_"
+    2.times { diagonal_board[5] << "_" }
+    if check_rows(string, 6, diagonal_board, row_counter = 0) 
+      true
+    else 
+      check_columns(string, 6, diagonal_board, row_counter = 0) ? true : false
+    end
+  end
+  
+  def play_again?
+    response = gets.chomp.downcase
+    case response
+    when "yes" then ConnectFour.new.game
+    when "no" then exit
+    else puts "*** Invalid entry.  Enter 'yes' or 'no'. ***"
+      play_again?
+    end
+  end
+  
+end
+
+ConnectFour.new.game
+
